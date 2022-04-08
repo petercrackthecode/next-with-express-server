ae00e6c1d5fe60924056cfb0134f405eb587ad46
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-04-08 00:51:48 -0700

    slightly change the message to trigger a redeployment

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,5 +2,5 @@
 import { withApiAuthRequired } from "@auth0/nextjs-auth0"
 
 export default withApiAuthRequired(function handler(req, res) {
-  res.status(200).json({ name: "John Doe" })
+  res.status(200).json({ name: "Peter Nguyen" })
 })
