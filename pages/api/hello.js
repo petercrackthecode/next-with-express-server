1b4870b482a5d21a41a1edb9a5ed01d4cbb71902
petercrackthecode <nguyenvandung02051999@gmail.com>
2022-04-08 00:53:34 -0700

    expose `/api/hello` publicly to test on aws

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { withApiAuthRequired } from "@auth0/nextjs-auth0"
 
-export default withApiAuthRequired(function handler(req, res) {
+export default function handler(req, res) {
   res.status(200).json({ name: "Peter Nguyen" })
-})
+}
